acf803f826e394135c66bcd052fefda933a08ac6
hyunlee214 <hyun110214@naver.com>
2021-08-21 15:29:34 +0900

    ejs파일작성 + 렌더링완료

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/sign_up', function(req, res, next) {
+  res.render('user/signup');
 });
+
+router.post('/sign_up', function(req, res, next) {
+  let body = req.body;
+
+  models.user.create({
+    name: body.userName,
+    email: body.userEmail,
+    password: body.password
+  })
+  .then( result => {
+    res.redirect('/users/sign_up');
+  })
+  .catch( err => {
+    console.log(err)
+  })
+})
 
 module.exports = router;
