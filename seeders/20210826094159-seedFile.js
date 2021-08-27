0b696a34618a9722e0a49c6ca1c8f258a2a46e7a
hyunlee214 <hyun110214@naver.com>
2021-08-27 13:24:53 +0900

    Seeders로 테스트데이터추가

--- seeders/20210826094159-seedFile.js
+++ seeders/20210826094159-seedFile.js
@@ -2,15 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+
+    let datas = [];
+    for (let i = 0; i < 3; i++) {
+      let testing = {
+        name : "testUser" + i,
+        email : "test" + i + "@testing.com",
+        password: "0000",
+        createdAt : new Date().toISOString().replace(/T/,'  ').replace(/\..+/, ''),
+        updatedAt : new Date().toISOString().replace(/T/,'  ').replace(/\..+/, '')
+      }
+      datas.push(testing);
+    }
+    
+    return queryInterface.bulkInsert('users', datas, {});
   },
 
   down: async (queryInterface, Sequelize) => {
