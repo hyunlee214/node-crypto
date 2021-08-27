9628f3182690a6be393026675cd6a02df49658a4
hyunlee214 <hyun110214@naver.com>
2021-08-27 17:35:56 +0900

    유사검색기능=> [/likeSearch/검색어]

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+
+
+// sequelize query - 유사 검색
+const express = require('express');
+const models = require('../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
+
+const router = express.Router();
+
+router.get("/likeSearch/:searchWord", function(req, res, next){
+  let searchWord = req.params.searchWord  
+
+  models.test.findAll({
+      where:{
+          postName: {
+              [Op.like]: "%" + searchWord + "%"
+          }
+      }
+  })
+      .then( result => {
+          res.json(result)
+      })
+      .catch( err => {
+          console.log(err)
+      })
+})
 
 module.exports = router;
