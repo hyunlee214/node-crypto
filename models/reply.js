b017cff846b9ac33a5a548292607f95b524ec209
hyunlee214 <hyun110214@naver.com>
2021-08-31 18:57:41 +0900

    reply모델 post와 매핑

--- models/reply.js
+++ models/reply.js
@@ -13,7 +13,13 @@ module.exports = (sequelize, DataTypes) => {
     content : {
       type: DataTypes.TEXT,
       allowNull: false,
-    }
+    }    
   });
+  // reply(댓글)과 post(게시글) 매핑
+  reply.associate = function(models){
+    reply.belongsTo(models.post, {
+      foreignKey: "postId"
+    })
+  };
   return reply;
 }
