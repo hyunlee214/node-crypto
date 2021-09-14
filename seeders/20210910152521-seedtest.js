8382d0334c6a21003bf60239dcb0825da8209d06
hyunlee214 <hyun110214@naver.com>
2021-09-14 09:51:08 +0900

    seed 활용해서 db관리

--- seeders/20210910152521-seedtest.js
+++ seeders/20210910152521-seedtest.js
@@ -8,21 +8,23 @@ module.exports = {
       let obj = {
         email: "seed" + x + "@seeding.com",
         name: "seeding" + x,
-        password: "0000",
+        password: "1234",
         createdAt : new Date().toISOString().replace(/T/,'  ').replace(/\..+/, ''),
         updatedAt : new Date().toISOString().replace(/T/,'  ').replace(/\..+/, '')
       }
       seeding.push(obj);
     }
     return queryInterface.bulkInsert('users',datas, {});
+
+    // sequelize db:migrate
+    // sequelize db:seed:all 
+    // seed 파일 작성 후 db에 반영 + seeder가 up의 로직 수행
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('users', null, {});
+
+    // sequelize db:seed:undo:all 
+    // seed를 초기화
   }
 };
